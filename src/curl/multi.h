b2e1507570c9d7bd09ca1448e33f121279053455
RayD <116186005+rpdistiso@users.noreply.github.com>
2024-12-05 16:56:05 +0000

    Optimize

--- src/curl/multi.h
+++ src/curl/multi.h
@@ -1,28 +1,5 @@
 #ifndef CURLINC_MULTI_H
 #define CURLINC_MULTI_H
-/***************************************************************************
- *                                  _   _ ____  _
- *  Project                     ___| | | |  _ \| |
- *                             / __| | | | |_) | |
- *                            | (__| |_| |  _ <| |___
- *                             \___|\___/|_| \_\_____|
- *
- * Copyright (C) Daniel Stenberg, <daniel@haxx.se>, et al.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution. The terms
- * are also available at https://curl.se/docs/copyright.html.
- *
- * You may opt to use, copy, modify, merge, publish, distribute and/or sell
- * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- * SPDX-License-Identifier: curl
- *
- ***************************************************************************/
 /*
   This is an "external" header file. Do not give away any internals here!
 
